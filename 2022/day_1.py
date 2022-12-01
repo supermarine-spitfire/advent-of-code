8e83249fe807741054f01037c82527bc9dbe87a1
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2022-12-01 02:48:22 -0500

    Finished Day 1.
    Worked on Part 2.

--- 2022/day_1.py
+++ 2022/day_1.py
@@ -16,8 +16,15 @@ for calorie_count in calorie_counts:
         total_calories.append(calorie_sum)
         calorie_sum = 0
 
-# Get maximum calorie count.
+# Get highest calorie count.
 total_calories.sort()
-print (f"len(calorie_counts): {len(calorie_counts)}")
-print (f"len(total_calories): {len(total_calories)}")
-print(f"Highest calorie count: {total_calories[-1]}")
+print("PART 1")
+print("======")
+print(f"Highest calorie count: {total_calories[0]}")
+print("======")
+
+# Get sum of top three calorie counts.
+print("PART 2")
+print("======")
+top_three_calorie_sum = total_calories.pop() + total_calories.pop() + total_calories.pop()
+print(f"Top three calorie sum: {top_three_calorie_sum}")
