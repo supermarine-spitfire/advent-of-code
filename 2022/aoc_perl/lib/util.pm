eaa0a3da99f29184722a072ade325f631e2cde55
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2022-12-06 00:47:41 -0500

    Defined Perl hash-printing subroutine.
    Useful for debugging.

--- 2022/aoc_perl/lib/util.pm
+++ 2022/aoc_perl/lib/util.pm
@@ -6,7 +6,7 @@ use autodie;
 
 use parent qw(Exporter);
 
-our @EXPORT = qw(file_to_array println);
+our @EXPORT = qw(file_to_array println print_hash);
 
 sub file_to_array {
     my %args = (
@@ -29,6 +29,21 @@ sub file_to_array {
 
 sub println {
     print(@_, "\n");
+}
+
+sub print_hash {
+    my %args = (
+        HASH_TO_PRINT => {},
+        HASH_NAME => "",
+        @_
+    );
+    my $hash_ref = $args{HASH_TO_PRINT};
+    my $hash_name = $args{HASH_NAME};
+
+    println("Contents of $hash_name:");
+    for my $key (keys %{$hash_ref}) {
+        println("$key => ${$hash_ref}{$key}");
+    }
 }
 
 1;
