6642b2dbe2860302f972d557d4b797642a1b9ff4
supermarine-spitfire <45949204+supermarine-spitfire@users.noreply.github.com>
2022-12-08 17:44:06 -0500

    Continued work on Day 7 Part 2.
    Works on small test case but not on actual input.

--- 2022/aoc_python/day_7.py
+++ 2022/aoc_python/day_7.py
@@ -37,57 +37,71 @@ class Directory:
 
     def calculate_sizes(self):
         """Populates the directory_size fields of each Directory."""
-        print(f"\nIn {self.current_directory}")
+        # print(f"\nIn {self.current_directory}")
         total_size = self.get_size_of_contents()
-        print(f"{self.current_directory}'s files' size: {total_size}")
+        # print(f"{self.current_directory}'s files' size: {total_size}")
 
         if not self.children:
             # Base case.
             # print("Base case.")
-            print(f"total_size for items within {self.current_directory}: {total_size}")
+            # print(f"total_size for items within {self.current_directory}: {total_size}")
             self.directory_size = total_size
             return total_size
         else:
             # Recursive case.
-            print("Recursive case.")
+            # print("Recursive case.")
             for child in self.children:
                 total_size += child.calculate_sizes()
-            print(f"\ntotal_size for items within {self.current_directory} (including items in subdirectories): {total_size}")
+            # print(f"\ntotal_size for items within {self.current_directory} (including items in subdirectories): {total_size}")
             self.directory_size = total_size
             return total_size
 
     def sum_filtered_sizes(self, upper_limit):
         """Returns the sum of the directory sizes that are less than or equal to upper_limit."""
-        print(f"\nIn {self.current_directory}")
+        # print(f"\nIn {self.current_directory}")
         total_size = self.directory_size if self.directory_size <= upper_limit else 0
-        print(f"{self.current_directory}'s total_size (0 if greater than {upper_limit}): {total_size}")
+        # print(f"{self.current_directory}'s total_size (0 if greater than {upper_limit}): {total_size}")
 
         if not self.children:
             # Base case.
-            print("Base case.")
-            print(f"total_size for items within {self.current_directory}: {total_size}")
+            # print("Base case.")
+            # print(f"total_size for items within {self.current_directory}: {total_size}")
             return total_size
         else:
             # Recursive case.
-            print("Recursive case.")
+            # print("Recursive case.")
             for child in self.children:
                 total_size += child.sum_filtered_sizes(upper_limit)
-            print(f"\ntotal_size for items within {self.current_directory}: {total_size}")
+            # print(f"\ntotal_size for items within {self.current_directory}: {total_size}")
             return total_size
 
     def get_smallest_dir(self, lower_limit):
         """Find smallest directory that is equal to or greater than lower_limit."""
-        total_size = self.directory_size
-
+        # print(f"Current directory: {self.current_directory}")
+        # print(f"lower_limit: {lower_limit}")
         if not self.children:
             # Base case.
-            print("Base case.")
-            return total_size
+            # print("\tBase case.")
+            # print(f"\tself.directory_size: {self.directory_size}")
+            return self.directory_size
         else:
              # Recursive case.
-            print("Recursive case.")
+            # print("Recursive case.")
+            smallest_size = self.directory_size
             for child in self.children:
-                pass
+                child_size = child.get_smallest_dir(lower_limit)
+
+                # print(f"smallest_size: {smallest_size}")
+                # print(f"size of {child.current_directory}: {child_size}")
+                # print(f"child_size >= lower_limit: {child_size >= lower_limit}")
+                # print(f"child_size > smallest_size: {child_size > smallest_size}")
+
+                if child_size >= lower_limit:
+                    # print("Updating smallest_size.")
+                    # print(f"{child_size} < {smallest_size} and > {lower_limit}.")
+                    smallest_size = child_size
+
+            return smallest_size
 
 
     def __str__(self):
@@ -111,9 +125,9 @@ size of contents: {self.get_size_of_contents()}
 
 print("Advent of Code 2022 Day 7")
 print("-------------------------")
-# terminal_output = io.file_to_list("input/day-7-test-data.txt")
+terminal_output = io.file_to_list("input/day-7-test-data.txt")
 # terminal_output = io.file_to_list("input/day-7-test-data-2.txt")
-terminal_output = io.file_to_list("input/day-7-input.txt")
+# terminal_output = io.file_to_list("input/day-7-input.txt")
 terminal_output = [s.split(" ") for s in terminal_output]
 
 # Identify all files and directories, along with their sizes.
@@ -177,14 +191,14 @@ for line in terminal_output:
 print(f"root\n{root}")
 
 # Traverse the directory structure.
-print("Running calculate_sizes().")
-total_size = root.calculate_sizes()
-print("Running sum_filtered_sizes().")
+# print("Running calculate_sizes().")
+total_directory_size = root.calculate_sizes()
+# print("Running sum_filtered_sizes().")
 total_filtered_size = root.sum_filtered_sizes(upper_limit=100000)
 
 print("PART 1")
 print("======")
-print(f"Total size of root: {total_size}")
+print(f"Total size of root: {total_directory_size}")
 print(f"Total size of all directories of at most 100000: {total_filtered_size}")
 print("======")
 # Attempt 1: 1855262 (too high)
@@ -192,4 +206,12 @@ print("======")
 
 total_disk_space = 70000000
 minimum_required_disk_space = 30000000
-
+total_unused_disk_space = total_disk_space - total_directory_size
+minimum_disk_space_to_free = minimum_required_disk_space - total_unused_disk_space
+dir_to_delete_size = root.get_smallest_dir(lower_limit=minimum_disk_space_to_free)
+print("PART 2")
+print("======")
+print(f"Minimum additional free disk space required for update: {minimum_disk_space_to_free}")
+print(f"Smallest size of directory to delete: {dir_to_delete_size}")
+print("======")
+# Attempt 1: 6721126 (too high)
