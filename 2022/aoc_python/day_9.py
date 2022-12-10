0dc9a8c048a832687090892da74b0989633c06f5
supermarine-spitfire <45949204+supermarine-spitfire@users.noreply.github.com>
2022-12-09 19:50:22 -0500

    Current progress on Day 9 Part 2.

--- 2022/aoc_python/day_9.py
+++ 2022/aoc_python/day_9.py
@@ -48,10 +48,55 @@ class Point2D:
     __repr__ = __str__
 
 
+def calculate_moves(head_position, tail_position, direction):
+    # Move the head.
+    previous_head_position = Point2D(head_position.x, head_position.y)
+    if direction == "U":
+        head_position.y += 1
+        # print(f"Moving head up: {head_position}")
+    elif direction == "D":
+        head_position.y -= 1
+        # print(f"Moving head down: {head_position}")
+    elif direction == "L":
+        head_position.x -= 1
+        # print(f"Moving head left: {head_position}")
+    elif direction == "R":
+        head_position.x += 1
+        # print(f"Moving head right: {head_position}")
+    else:
+        pass    # Ignore any other input.
+
+    # print(f"Previous head position: {previous_head_position}")
+    # Move the tail, if needed.
+    # The distance checks test if head and tail are touching or not. Tail moves if the test fails.
+    # The tail moves to the previous position of the head.
+    if head_position.euclidean_distance(tail_position) >= 2  \
+       or head_position.manhattan_distance(tail_position) >= 3:
+       # Euclidean distance handles cases where head and tail are in same rows/columns.
+       # Manhattan distance handles cases where head and tail are on diagonals.
+       tail_position = previous_head_position
+
+    return head_position, tail_position
+
+
+def print_tail_positions(max_x, max_y, covered_positions):
+    for i in range(max_y, -1, -1):  # Printing from top on down.
+        for j in range(max_x + 2):  # Add buffer at end of row.
+            cur_point = Point2D(j, i)
+            if cur_point == origin:
+                print("s", end=" ")
+            elif cur_point in covered_positions:
+                print("#", end=" ")
+            else:
+                print(".", end=" ")
+        print()
+
+
 print("Advent of Code 2022 Day 9")
 print("-------------------------")
 # file_input = io.file_to_list("input/day-9-test-data.txt")
-file_input = io.file_to_list("input/day-9-input.txt")
+file_input = io.file_to_list("input/day-9-test-data-2.txt")
+# file_input = io.file_to_list("input/day-9-input.txt")
 rope_head_moves = []
 
 for line in file_input:
@@ -61,69 +106,27 @@ origin = Point2D(0, 0)          # Map everything relative to this point.
 head_position = Point2D(0, 0)
 tail_position = Point2D(0, 0)   # Tail starts out overlapped by head.
 covered_positions = []
-max_x = 0
-max_y = 0
+# max_x = 0
+# max_y = 0
 for move in rope_head_moves:
-    # print(f"move: {move}")
-    # print(f"covered_positions: {covered_positions}")
-    # print(f"current head position: {head_position}")
-    # print(f"current tail position: {tail_position}")
     direction = move[0]
     num_steps = int(move[1])
-    max_x = num_steps if direction == "U" and num_steps > max_x else max_x
-    max_y = num_steps if direction == "R" and num_steps > max_y else max_y
+    # max_x = num_steps if direction == "U" and num_steps > max_x else max_x
+    # max_y = num_steps if direction == "R" and num_steps > max_y else max_y
 
     for i in range(num_steps):
-        # Move the head.
-        previous_head_position = Point2D(head_position.x, head_position.y)
-        if direction == "U":
-            head_position.y += 1
-            # print(f"Moving head up: {head_position}")
-        elif direction == "D":
-            head_position.y -= 1
-            # print(f"Moving head down: {head_position}")
-        elif direction == "L":
-            head_position.x -= 1
-            # print(f"Moving head left: {head_position}")
-        elif direction == "R":
-            head_position.x += 1
-            # print(f"Moving head right: {head_position}")
-        else:
-            continue    # Ignore any other input.
-        # print(f"Previous head position: {previous_head_position}")
-
-        # Move the tail, if needed.
-        # The distance checks test if head and tail are touching or not. Tail moves if the test fails.
-        # The tail moves to the previous position of the head.
-        if head_position.euclidean_distance(tail_position) >= 2  \
-           or head_position.manhattan_distance(tail_position) >= 3:
-           # Euclidean distance handles cases where head and tail are in same rows/columns.
-           # Manhattan distance handles cases where head and tail are on diagonals.
-           if tail_position not in covered_positions:
-                covered_positions.append(Point2D(tail_position.x, tail_position.y))
-           tail_position = previous_head_position
-        #    print("Moving tail to previous head position.")
-        #    print(f"New tail position: {tail_position}")
-        #    print(f"covered_positions: {covered_positions}")
-        # print()
+        head_position, tail_position = calculate_moves(head_position=head_position, tail_position=tail_position, direction=direction)
+        if tail_position not in covered_positions:
+            covered_positions.append(Point2D(tail_position.x, tail_position.y))
 
 if tail_position not in covered_positions:
     covered_positions.append(tail_position) # Count the tail's position once the loop ends.
 
-print(f"covered_positions (final): {covered_positions}")
-print(f"max_x: {max_x}")
-print(f"max_y: {max_y}")
+# print(f"covered_positions (final): {covered_positions}")
+# print(f"max_x: {max_x}")
+# print(f"max_y: {max_y}")
 # Print human-readable version of covered_positions.
-for i in range(max_y, -1, -1):  # Printing from top on down.
-    for j in range(max_x + 2):  # Add buffer at end of row.
-        cur_point = Point2D(j, i)
-        if cur_point == origin:
-            print("s", end=" ")
-        elif cur_point in covered_positions:
-            print("#", end=" ")
-        else:
-            print(".", end=" ")
-    print()
+# print_tail_positions(max_x, max_y, covered_positions)
 
 num_tail_positions_visited = len(covered_positions)
 print("PART 1")
@@ -132,3 +135,33 @@ print(f"Number of distinct positions visited by rope tail: {num_tail_positions_v
 print("======")
 # Attempt 1: 8986 (too high)
 # Attempt 2: 6266
+
+# max_x = 0
+# max_y = 0
+covered_positions.clear()
+rope_knots = [Point2D(0, 0) for i in range(10)] # Make a ten-knot rope.
+tail_index = len(rope_knots) - 1
+for move in rope_head_moves:
+    direction = move[0]
+    num_steps = int(move[1])
+    # max_x = num_steps if direction == "U" and num_steps > max_x else max_x
+    # max_y = num_steps if direction == "R" and num_steps > max_y else max_y
+    for i in range(num_steps):
+        j = 0
+        while j < tail_index:   # Stops one knot ahead of the rope's tail.
+            cur_head = rope_knots[j]
+            cur_tail = rope_knots[j + 1]
+            cur_head, cur_tail = calculate_moves(head_position=cur_head, tail_position=cur_tail, direction=direction)
+
+            if j + 1 == tail_index and cur_tail not in covered_positions:
+                # cur_tail is the tail of the rope; record its current position if new.
+                covered_positions.append(Point2D(cur_tail.x, cur_tail.y))
+
+            j += 1
+
+print(f"covered_positions (final): {covered_positions}")
+num_tail_positions_visited = len(covered_positions)
+print("PART 2")
+print("======")
+print(f"Number of distinct positions visited by rope tail: {num_tail_positions_visited}")
+print("======")
