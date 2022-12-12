828bda1c675a47a2fd519c4954b0b5d944f19688
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2022-12-12 02:48:28 -0500

    Defined function file_dimensions().
    This function returns the number of rows and columns in a text file.

--- 2022/aoc_python/lib/io.py
+++ 2022/aoc_python/lib/io.py
@@ -13,3 +13,11 @@ def file_to_string(filename):
     """Returns the contents of a text file as one string."""
     with open(resolve_filepath(filename)) as f:
         return f.read()
+
+def file_dimensions(filename):
+    """Returns the number of columns and rows in a text file.
+
+        Assumes all rows are the same length and the first line is non-empty.
+    """
+    file_contents = file_to_list(filename)
+    return len(file_contents), len(file_contents[0])
