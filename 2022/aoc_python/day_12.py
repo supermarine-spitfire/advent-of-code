1b152700a5a84bb88ca448acafef348413c689c8
supermarine-spitfire <45949204+supermarine-spitfire@users.noreply.github.com>
2022-12-12 15:09:55 -0500

    Current progress on Day 12.
    Defined class DirectedGraph for operations required in Part 1.
    
    Construction of the graphs appears correct, but breadth-first search is not.

--- 2022/aoc_python/day_12.py
+++ 2022/aoc_python/day_12.py
@@ -1,78 +1,117 @@
 from lib import io
-from lib.collections import Graph, Vertex
+from lib.collections import DirectedGraph, Vertex
 
 print("Advent of Code 2022 Day 12")
 print("-------------------------")
 
-height_map_string = io.file_to_string("input/day-12-test-data.txt")
-height_map = io.file_to_list("input/day-12-test-data.txt")
-num_height_map_rows, num_height_map_cols = io.file_dimensions("input/day-12-test-data.txt")
+testing = True
+if testing:
+    height_map_string = io.file_to_string("input/day-12-test-data.txt")
+    height_map = io.file_to_list("input/day-12-test-data.txt")
+    num_rows, num_cols = io.file_dimensions("input/day-12-test-data.txt")
+else:
+    height_map_string = io.file_to_string("input/day-12-input.txt")
+    height_map = io.file_to_list("input/day-12-input.txt")
+    num_rows, num_cols = io.file_dimensions("input/day-12-input.txt")
+# print(f"File dimensions: {num_rows}, {num_cols}")
 
 def is_traversable(start_char, end_char):
+    # print("In is_traversable().")
+    # print(f"start_char: {start_char}")
+    # print(f"end_char: {end_char}")
     # Convert the starting and ending point characters to their heights first.
     if start_char == "S":
         start_char = "a"
+        # print(f"start_char (after conversion): {start_char}")
     elif start_char == "E":
         start_char = "z"
+        # print(f"start_char (after conversion): {start_char}")
 
     if end_char == "S":
         end_char = "a"
+        # print(f"end_char (after conversion): {end_char}")
     elif end_char == "E":
         end_char = "z"
-    return ord(end_char) - ord(start_char) <= 1
+        # print(f"end_char (after conversion): {end_char}")
+    # print("Exiting is_traversable().")
+    # return ord(end_char) - ord(start_char) <= 1 # End must be at most 1 unit higher than start.
+    height_difference = ord(end_char) - ord(start_char)
+    if abs(height_difference) <= 1:
+        return 1    # Can go in both directions between start and end.
+    elif height_difference < 0:
+        return 0    # Can only go from start to end.
+    else:
+        return -1   # Not possible to go from start to end.
 
 def connect_squares(graph, starting_square, ending_square):
-    if ending_square and is_traversable(starting_square.data, ending_square.data):
+    # print("In connect_squares().")
+    if ending_square:
         graph.add_vertex(ending_square)
-        graph.add_edge(starting_square, ending_square)
+        edge_type = is_traversable(starting_square.data, ending_square.data)
+        if edge_type == 1:
+            # print(f"Making edges in both directions between {starting_square} and {ending_square}.")
+            # Connect starting_square and ending_square in both directions.
+            graph.add_edge(starting_square, ending_square)
+            graph.add_edge(ending_square, starting_square)
+        elif edge_type == 0:
+            # print(f"Making edge from {starting_square} to {ending_square}.")
+            # Only connect starting_square to ending_square
+            graph.add_edge(starting_square, ending_square)
+        elif edge_type == -1:
+            pass
+
+    # print("Exiting connect_squares().\n")
 
 # Construct graph representing all connected grid squares.
-accessible_areas = Graph()
+accessible_areas = DirectedGraph()
 for i in range(len(height_map)):
     for j in range(len(height_map[i])):
         # print(f"i: {i}")
         # print(f"j: {j}")
-        print(f"height_map[{i}][{j}]: {height_map[i][j]}")
-        cur_square = Vertex(num_height_map_rows * i + j, height_map[i][j])
-        print(f"cur_square: {cur_square}")
+        # print(f"height_map[{i}][{j}]: {height_map[i][j]}")
+        cur_square = Vertex(num_cols * i + j, height_map[i][j])
+        # print(f"cur_square: {cur_square}")
         accessible_areas.add_vertex(cur_square)
 
         # Check each of height_map[i][j]'s neighbours.
         # If neighbour is accessible, add it to the graph.
+        northern_label = num_cols * (i - 1) + j
         northern_neighbour = Vertex(
-            num_height_map_rows * (i - 1) + j,
+            northern_label,
             height_map[i - 1][j]
         ) if i - 1 >= 0 else None
+        southern_label = num_cols * (i + 1) + j
         southern_neighbour = Vertex(
-            num_height_map_rows * (i + 1) + j,
+            southern_label,
             height_map[i + 1][j]
         ) if i + 1 < len(height_map) else None
+        eastern_label = num_cols * i + (j + 1)
         eastern_neighbour = Vertex(
-            num_height_map_rows * i + (j + 1),
+            eastern_label,
             height_map[i][j + 1]
         ) if j + 1 < len(height_map[i]) else None
+        western_label = num_cols * i + (j - 1)
         western_neighbour = Vertex(
-            num_height_map_rows * i + (j - 1),
+            western_label,
             height_map[i][j - 1]
         ) if j - 1 >= 0 else None
-
-        # print(f"northern_neighbour: {northern_neighbour}")
-        # print(f"southern_neighbour: {southern_neighbour}")
-        # print(f"eastern_neighbour: {eastern_neighbour}")
-        # print(f"western_neighbour: {western_neighbour}")
-        # print()
 
         connect_squares(accessible_areas, cur_square, northern_neighbour)
         connect_squares(accessible_areas, cur_square, southern_neighbour)
         connect_squares(accessible_areas, cur_square, eastern_neighbour)
         connect_squares(accessible_areas, cur_square, western_neighbour)
 
-print("accessible_areas:")
-print(accessible_areas)
+# print("accessible_areas:")
+# print(accessible_areas)
 
 # Run shortest path algorithm on graph from S to E.
 current_position = accessible_areas.get_vertex(height_map_string.replace("\n", "").find("S"))
 destination = accessible_areas.get_vertex(height_map_string.replace("\n", "").find("E"))
-print(f"current_position: {current_position}")
-print(f"destination: {destination}")
+print(f"current_position (before search): {current_position}")
+print(f"destination (before search): {destination}")
 accessible_areas.bfs(current_position)
+path = []
+accessible_areas.get_path(current_position, destination, path)
+print(f"current_position (after search): {current_position}")
+print(f"destination (after search): {destination}")
+print(f"path: {path}")
--- 2022/aoc_python/lib/collections.py
+++ 2022/aoc_python/lib/collections.py
@@ -1,112 +1,8 @@
 import sys
 
+from abc import abstractclassmethod
+from collections import deque
 from enum import Enum
-from queue import Queue
-
-class Graph:
-    def __init__(self):
-        self.adjacency_lists = {}
-        self.vertices = []
-
-    def __str__(self) -> str:
-        return f"""Vertices: {sorted(self.vertices)}
-Adjacency lists: {self.adjacency_lists}
-"""
-
-    __repr__ = __str__
-
-    def add_vertex(self, v):
-        # Returns boolean flag indicating if operation succeeded or not.
-        if v not in self.vertices:
-            self.adjacency_lists[v]= set()
-            self.vertices.append(v)
-            return True
-        else:
-            return False
-
-    def remove_vertex(self, v):
-        # Returns boolean flag indicating if operation succeeded or not.
-        if v in self.vertices:
-            del self.adjacency_lists[v]
-            self.vertices.remove(v)
-            return True
-        else:
-            return False
-
-    def get_vertex(self, label):
-        # Returns None if no vertices have the provided label.
-        for v in self.vertices:
-            if v.label == label:
-                return v
-        return None
-
-    def vertex_in_graph(self, label):
-        return label in map(lambda v: v.label, self.vertices)
-
-    def add_edge(self, source, target):
-        # Returns boolean flag indicating if operation succeeded or not.
-        if source not in self.vertices:
-            return False
-        elif source in self.adjacency_lists.keys():
-            self.adjacency_lists[source].add(target)
-            return True
-        else:
-            self.adjacency_lists[source] = set().add(target)
-            return True
-
-    def remove_edge(self, source, target):
-        # Returns boolean flag indicating if operation succeeded or not.
-        if source in self.adjacency_lists.keys():
-            self.adjacency_lists[source].remove(target)
-            return True
-        else:
-            return False
-
-    def is_adjacent(self, source, target):
-        return target in self.adjacency_lists[source]
-
-    def neighbours(self, v):
-        return set(self.adjacency_lists[v]) if v in self.adjacency_lists.keys() else set()
-
-    def bfs(self, source):
-        # Implements breadth-first search.
-        print("In Graph.bfs().")
-        if not self.vertex_in_graph(source.label):
-            return False
-
-        vertices = self.vertices.copy()
-        vertices.remove(source)
-        print(f"vertices: {vertices}")
-        for v in vertices:
-            v.colour = Vertex.Colour.WHITE
-            v.distance = sys.maxsize
-            v.predecessor = None
-        
-        source.colour = Vertex.Colour.GREY
-        source.distance = 0
-        source.predecessor = None
-
-        vertices_to_process = Queue()
-        vertices_to_process.put(source)
-        while not vertices_to_process.empty():
-            u = vertices_to_process.get()
-            for v in self.neighbours(u):
-                if v.colour == Vertex.Colour.WHITE: # v has not been visited yet.
-                    v.colour = Vertex.Colour.GREY # Add to frontier; there may be neighbouring unvisited vertices.
-                    v.distance = u.distance + 1
-                    v.predecessor = u
-                    vertices_to_process.put(v)
-            u.colour = Vertex.Colour.BLACK # All neighbouring vertices have been visited.
-
-        return True
-
-    def get_path(self, source, target, list_of_edges=[]):
-        if source == target:
-            return [source]
-        elif target.predecessor is None:
-            return []
-        else:
-            return self.get_path(source, target.predecessor, list_of_edges.append(target))
 
 
 class Vertex:
@@ -152,6 +48,155 @@ class Vertex:
             return self.__gt__(other) or self.__eq__(other)
 
         def __str__(self) -> str:
-            return f"Vertex({self.label}, {self.data})"
+            return   f"Vertex(label: {self.label}, data: {self.data}, " \
+                   + f"predecessor: ({self.predecessor.label if self.predecessor else None}, " \
+                   + f"{self.predecessor.data if self.predecessor else None}), " \
+                   + f"colour: {self.colour if self.colour else None}, " \
+                   + f"distance: {self.distance if self.distance else None})"
 
         __repr__ = __str__
+
+
+class Graph:
+    def __init__(self):
+        self.adjacency_lists = {}
+        self.vertices = []
+
+    def __str__(self) -> str:
+        s = f"Vertices: {sorted(self.vertices)}"+ "\nAdjacency lists:\n"
+        for vertex, edges in self.adjacency_lists.items():
+            s += f"({vertex.label}, {vertex.data}): {edges}\n"
+        return s
+
+    __repr__ = __str__
+
+    @abstractclassmethod
+    def add_vertex(self, v):
+       pass
+
+    @abstractclassmethod
+    def remove_vertex(self, v):
+        pass
+
+    def get_vertex(self, label):
+        # Returns None if no vertices have the provided label.
+        for v in self.vertices:
+            if v.label == label:
+                return v
+        return None
+
+    def vertex_in_graph(self, label):
+        return label in map(lambda v: v.label, self.vertices)
+
+    @abstractclassmethod
+    def add_edge(self, source, target):
+        pass
+
+    @abstractclassmethod
+    def remove_edge(self, source, target):
+        pass
+
+    def is_adjacent(self, source, target):
+        return target in self.adjacency_lists[source]
+
+    def neighbours(self, v):
+        return set(self.adjacency_lists[v]) if v in self.adjacency_lists.keys() else set()
+
+    def reset_vertices(self):
+        for v in self.vertices:
+            v.colour = None
+            v.distance = None
+            v.predecessor = None
+
+    def bfs(self, source):
+        # Implements breadth-first search.
+        print("In Graph.bfs().")
+        print(self)
+        if not self.vertex_in_graph(source.label):
+            return False
+
+        vertices = self.vertices.copy()
+        vertices.remove(source)
+        for v in vertices:
+            v.colour = Vertex.Colour.WHITE  # Unvisited.
+            v.distance = sys.maxsize        # Effectively infinitely far from source.
+            v.predecessor = None
+            # print(f"v: {v}")
+        print(f"vertices: {sorted(vertices)}")
+
+        source.colour = Vertex.Colour.GREY  # Visited by default; has neighbouring unvisited vertices.
+        source.distance = 0
+        source.predecessor = None
+        print(f"source: {source}\n")
+
+        vertices_to_process = deque()
+        vertices_to_process.append(source)
+        while vertices_to_process:
+            print(f"vertices_to_process: {vertices_to_process}")
+            u = vertices_to_process.popleft()
+            print(f"u: {u}")
+            for v in self.neighbours(u):
+                print(f"v (neighbour of u): {v}")
+                if v.colour == Vertex.Colour.WHITE: # v has not been visited yet.
+                    v.colour = Vertex.Colour.GREY   # Add to frontier; there may be neighbouring unvisited vertices.
+                    v.distance = u.distance + 1
+                    v.predecessor = u
+                    print(f"v discovered: {v}. Will process.")
+                    vertices_to_process.append(v)
+            u.colour = Vertex.Colour.BLACK          # All neighbouring vertices have been visited.
+            print(f"All neighbours of {u} discovered.\n")
+        print("Vertices after search:")
+        for v in sorted(self.vertices):
+            print(f"v: {v}")
+
+        return True
+
+
+class DirectedGraph(Graph):
+    def __init__(self):
+        super().__init__()
+
+    def add_vertex(self, v):
+        # Returns boolean flag indicating if operation succeeded or not.
+        if v not in self.vertices:
+            self.adjacency_lists[v]= set()
+            self.vertices.append(v)
+            return True
+        else:
+            return False
+
+    def remove_vertex(self, v):
+        # Returns boolean flag indicating if operation succeeded or not.
+        if v in self.vertices:
+            del self.adjacency_lists[v]
+            self.vertices.remove(v)
+            return True
+        else:
+            return False
+
+    def add_edge(self, source, target):
+        # Returns boolean flag indicating if operation succeeded or not.
+        if source not in self.vertices:
+            return False
+        elif source in self.adjacency_lists.keys():
+            self.adjacency_lists[source].add(target)
+            return True
+        else:
+            self.adjacency_lists[source] = set().add(target)
+            return True
+
+    def remove_edge(self, source, target):
+        # Returns boolean flag indicating if operation succeeded or not.
+        if source in self.adjacency_lists.keys():
+            self.adjacency_lists[source].remove(target)
+            return True
+        else:
+            return False
+
+    def get_path(self, source, target, list_of_edges):
+        if source == target:
+            return [source]
+        elif target.predecessor is None:
+            return []
+        else:
+            return self.get_path(source, target.predecessor, list_of_edges.append(target))
