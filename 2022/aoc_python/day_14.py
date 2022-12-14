7a362bc29af8b260f18578a99b3396d00ca06e3c
supermarine-spitfire <45949204+supermarine-spitfire@users.noreply.github.com>
2022-12-14 15:56:02 -0500

    Current status of Day 14.

--- 2022/aoc_python/day_14.py
+++ 2022/aoc_python/day_14.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from lib import io
-from lib.geometry import Line2D, Point2D
+from lib.geometry import LineSegment2D, Point2D
 
 class Direction(Enum):
     BLOCKED = -1
@@ -11,13 +11,17 @@ class Direction(Enum):
 
 def free_to_move(sand, walls, placed_sand):
     def is_free(point):
+        print("In is_free().")
+        print(f"point: {point}")
         # Check if it hits a wall.
         for wall in walls:
+            print(f"wall: {wall}")
             if wall.intersects(point):
                 return False
 
         # Check if it hits placed sand.
         for ps in placed_sand:
+            print(f"ps: {ps}")
             if ps.is_touching(point):
                 False
         return True
@@ -34,7 +38,7 @@ def free_to_move(sand, walls, placed_sand):
     else:
         return Direction.BLOCKED
 
-testing = False
+testing = True
 if testing:
     cave_scan = io.file_to_list("input/day-14-test-data.txt")
 else:
@@ -44,39 +48,58 @@ print("Advent of Code 2022 Day 14")
 print("-------------------------")
 
 # Construct a model of the cave given input. Said input defines the walls and floors (henceforth "wall"), through which sand cannot penetrate.
+max_y = 0   # Tracks the lowest cave wall in system. Sand that goes below this will fall for ever.
 cave_walls = []
 for line in cave_scan:
     # Each line defines a contiguous cave wall.
     # cave_wall = []
     segments = line.split("->")
     for i in range(len(segments) - 1):
-        start = segments[i].split(",")
-        end = segments[i + 1].split(",")
-        wall_segment = Line2D(
-            start=Point2D(int(start[0]), int(start[1])),
-            end=Point2D(int(end[0]), int(end[1])),
+        start_x, start_y = segments[i].split(",")
+        end_x, end_y = segments[i + 1].split(",")
+        start_x = int(start_x)
+        start_y = int(start_y)
+        end_x = int(end_x)
+        end_y = int(end_y)
+        wall_segment = LineSegment2D(
+            start=Point2D(start_x, start_y),
+            end=Point2D(end_x, end_y),
         )
+        max_y = start_y if start_y > max_y else max_y
+        max_y = end_y if end_y > max_y else max_y
         cave_walls.append(wall_segment)
         # cave_wall.append(wall_segment)
     # cave_walls.append(cave_wall)
+print(f"max_y: {max_y}")
 
 # Start pouring sand from point (500, 0).
 # Loop as long as a unit of sand can come to a rest.
 can_rest = True
 placed_sand_units = []
-while True:
+while can_rest:
     sand = Point2D(500, 0)
-    direction = free_to_move(sand, cave_walls, placed_sand_units)
+    direction = Direction.DOWN
     while direction != Direction.BLOCKED:
         direction = free_to_move(sand, cave_walls, placed_sand_units)
+        print(f"direction: {direction}")
         if direction == Direction.DOWN:
-            pass
+            sand.y += 1
         elif direction == Direction.LEFT:
-            pass
+            sand.x -= 1
+            sand.y += 1
         elif direction == Direction.RIGHT:
-            pass
+            sand.x += 1
+            sand.y += 1
+
+        print(f"sand.y: {sand.y}")
+        if sand.y > max_y:
+            # Sand is falling into the abyss; terminate.
+            can_rest = False
+
+    if can_rest:
+        placed_sand_units.append(sand)
 
 print("PART 1")
 print("======")
-print(f"Maximum number of sand units deposited before excess flows over:")
+print(f"Maximum number of sand units deposited before excess flows over: {len(placed_sand_units)}")
 print("======")
--- 2022/aoc_python/lib/geometry.py
+++ 2022/aoc_python/lib/geometry.py
@@ -6,6 +6,14 @@ class Point2D:
     def __str__(self):
         return f"Point2D({self.x}, {self.y})"
 
+    def __eq__(self, other):
+        if isinstance(other, Point2D):
+            return self.x == other.x and self.y == other.y
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
     def is_touching(self, p):
         if p.y == self.y:
             if p.x == self.x + 1 or p.x == self.x - 1:  # Horizontal cases.
@@ -21,7 +29,7 @@ class Point2D:
 
     __repr__ = __str__
 
-class Line2D:
+class LineSegment2D:
     def __init__(self, start, end):
         self.start = start
         self.end = end
@@ -30,12 +38,25 @@ class Line2D:
         return f"Line2D(start: {self.start}, end: {self.end}"
 
     def intersects(self, p):
-        if p.x == self.start.x and p.y >= self.start.y and p.y <= self.end.y:   # Vertical case.
+        if p.x < self.start.x or p.x > self.end.y or p.y < self.start.y or p.y > self.end.y: # Outside of line segment.
+            return False
+        if p == self.start or p == self.end:    # Same as endpoints.
+            return True
+        elif p.x == self.start.x and p.y >= self.start.y and p.y <= self.end.y:   # Vertical case.
             return True
         elif p.y == self.start.y and p.x >= self.start.x and p.x <= self.end.x: # Horizontal case.
             return True
         else:                                                                   # Oblique case.
-            m = (self.end.x - self.start.x) / (self.end.y - self.start.y)
-            return p.y - self.start.y == m * (p.x - self.start.x)
+            if self.start.y == self.end.y:
+                m = 0
+            elif self.start.x == self.end.x:
+                m = None
+            else:
+                m = (self.end.x - self.start.x) / (self.end.y - self.start.y)
+
+            if m:
+                return p.y - self.start.y == m * (p.x - self.start.x)
+            else:
+                return None # Should not happen.
 
     __repr__ = __str__
