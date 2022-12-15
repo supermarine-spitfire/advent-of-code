63fe50833b0b1cc2704900f0f9bf13702870f1c0
I Manuel <45949204+supermarine-spitfire@users.noreply.github.com>
2022-12-14 23:27:41 -0500

    Refactored Day 14.
    Collision calculations now use sets instead of lists and LineSegment2D.intersect(). Time savings is significant.

--- 2022/aoc_python/day_14.py
+++ 2022/aoc_python/day_14.py
@@ -11,20 +11,11 @@ class Direction(Enum):
 
 def free_to_move(sand, walls, placed_sand):
     def is_free(point):
-        # print("In is_free().")
-        # print(f"point: {point}")
-        # Check if it hits a wall.
-        for wall in walls:
-            # print(f"wall: {wall}")
-            if wall.intersects(point):
-                return False
-
-        # Check if it hits placed sand.
-        for ps in placed_sand:
-            # print(f"ps: {ps}")
-            if ps == point:
-                return False
-        return True
+        # Check if point hits a wall or placed sand.
+        if point in walls or point in placed_sand:
+            return False
+        else:
+            return True
 
     # First check if the point directly down is free.
     # If not free, check down left then down right.
@@ -49,7 +40,7 @@ print("-------------------------")
 
 # Construct a model of the cave given input. Said input defines the walls and floors (henceforth "wall"), through which sand cannot penetrate.
 max_y = 0   # Tracks the lowest cave wall in system. Sand that goes below this will fall for ever.
-cave_walls = []
+cave_walls = set()
 for line in cave_scan:
     # Each line defines a contiguous cave wall.
     # cave_wall = []
@@ -61,21 +52,24 @@ for line in cave_scan:
         start_y = int(start_y)
         end_x = int(end_x)
         end_y = int(end_y)
-        wall_segment = LineSegment2D(
-            start=Point2D(start_x, start_y),
-            end=Point2D(end_x, end_y),
-        )
+
+        if start_x == end_x:    # Vertical segment.
+            for y in range(min(start_y, end_y), max(start_y, end_y) + 1):
+                # print(f"Point: ({start_x}, {y})")
+                cave_walls.add(Point2D(start_x, y))
+        else:                   # Horizontal segment.
+            for x in range(min(start_x, end_x), max(start_x, end_x) + 1):
+                # print(f"Point: ({x}, {start_y})")
+                cave_walls.add(Point2D(x, start_y))
+
         max_y = start_y if start_y > max_y else max_y
         max_y = end_y if end_y > max_y else max_y
-        cave_walls.append(wall_segment)
-        # cave_wall.append(wall_segment)
-    # cave_walls.append(cave_wall)
-print(f"max_y: {max_y}")
+# print(f"max_y: {max_y}")
 
 # Start pouring sand from point (500, 0).
 # Loop as long as a unit of sand can come to a rest.
 can_rest = True
-placed_sand_units = []
+placed_sand_units = set()
 while can_rest:
     sand = Point2D(500, 0)
     direction = Direction.DOWN
@@ -98,10 +92,12 @@ while can_rest:
             break
 
     if can_rest:
-        placed_sand_units.append(sand)
+        placed_sand_units.add(sand)
 
 print("PART 1")
 print("======")
 print(f"Maximum number of sand units deposited before excess flows over: {len(placed_sand_units)}")
 print("======")
 # Attempt 1: 698
+# Time trial 1: 52.2 s
+# Time trial 2: 0.4 s
